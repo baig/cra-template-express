dde5eecf76e54f4b852c9f50f59f86eaf1fc36ec
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2020-01-19 18:30:39 +0800

    fix: database config name

--- sources/api/src/config/db.ts
+++ sources/api/src/config/db.ts
@@ -5,6 +5,6 @@ export const dbConfig = {
   port: env('DB_PORT', 3306),
   user: env('DB_USER', 'root'),
   pass: env('DB_PASS', ''),
-  name: env('DB_NAME', 'web_mvp'),
+  name: env('DB_NAME', 'node_mvp'),
   dialect: env('DB_DIALECT', 'mysql'),
 };
